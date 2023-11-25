f71b4801eee7f2715abca73d95533cab73e55fee
Ho Nyeong <dev.honing@gmail.com>
2023-11-25 22:38:38 +0900

    throw를 이용한 1번 예제 작성

--- throw.js
+++ throw.js
@@ -1,3 +1,13 @@
 // throw.js
 
 // throw 키워드 탐구하기
+console.log('1번 예제 시작');
+
+try {
+  throw new Error('에러 발생!');
+} catch (error) {
+  console.error('1번 예제에서의 예외 처리:', error.message);
+}
+
+console.log('1번 예제 종료');
+
